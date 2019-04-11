86c847c956297a3ef190c3fba5ac74bd2b03ee5f
Bleen <bleengrg12345@gmail.com>
2019-04-11 23:21:22 +0545

    Initial commit

---
+++ app/src/main/java/com/example/taxcalucator_bishal/Calculate.java
@@ -0,0 +1,42 @@
+package com.example.taxcalucator_bishal;
+
+public class Calculate {
+
+    private double Salary;
+
+
+    public double Two_lakh()
+    {
+        return (Salary*0.01)*12;
+    }
+
+    public double Three_half_lakh()
+    {
+        double remain = Salary - 200000;
+        double remain_tax = remain*0.15;
+        double main = 200000*0.01;
+        //double total = main + remain_tax + Salary;
+        double total = (main + remain_tax) * 12;
+        return total;
+    }
+
+    public double Above_three()
+    {
+        double remain = Salary - 200000;
+        double remain_after = remain - 150000;
+        double main = 200000*0.01;
+        double second_main = 150000*0.15;
+        double remain_tax = remain_after*0.25;
+        //double total = main + second_main + remain_tax + Salary;
+        double total = (main + second_main + remain_tax) * 12;
+        return total;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(double salary) {
+        Salary = salary;
+    }
+}
